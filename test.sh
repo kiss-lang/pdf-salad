e61568930ddbb8cfd65926a5a4bffa2f4fb2ca98
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-11-30 12:05:18 -0700

    move pdf-salad files to repo root

--- test.sh
+++ test.sh
@@ -1,29 +1,12 @@
 #! /bin/bash
 
-KISS_TARGET=${KISS_TARGET:-$1}
-KISS_TARGET=${KISS_TARGET:-interp}
-
-# Test projects with test-project.sh
-if [ -n "$KISS_PROJECT" ]
-then
-    ./test-project.sh
-# Test Kiss with utest cases in kiss/src/test/cases
-else
-    # If CI is running tests, basic dependencies need to be installed
-    if [ -n "$CI_OS_NAME" ]
-    then
-        git clone https://github.com/kiss-lang/kiss
-        (cd kiss && lix download)
-        if [ "$KISS_TARGET" = cpp ]; then
-            (cd kiss && lix install haxelib:hxcpp)
-        elif [ "$KISS_TARGET" = nodejs ]; then
-            (cd kiss && lix install haxelib:hxnodejs)
-        fi
-    fi
-
-    if [ ! -z "$2" ]; then
-        (cd kiss && haxe -D cases=$2 build-scripts/common-args.hxml build-scripts/common-test-args.hxml build-scripts/$KISS_TARGET/test.hxml)
-    else
-        (cd kiss && haxe build-scripts/common-args.hxml build-scripts/common-test-args.hxml build-scripts/$KISS_TARGET/test.hxml)
-    fi
+# Something is broken in the Node dependencies on Mac, so don't bother.
+if [ "$CI_OS_NAME" = "macos-latest" ]; then
+    exit 0
 fi
+
+if [ ! -z "$CI_OS_NAME" ]; then
+    npm install pdf-lib
+    haxelib install hxnodejs
+fi
+haxe build.hxml
